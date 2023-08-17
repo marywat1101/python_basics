6cc66759abe8d3ec7675e7ffa7586790469522aa
marywat1101 <mariamsaginadze1317@gmail.com>
2023-08-17 23:01:32 +0400

    finished third week notes

---
+++ Week3.py
@@ -0,0 +1,59 @@
+# print(len("jamilla"))
+
+# s = "meredith"
+# print(s[4])
+# print(s[3])
+# print(s[-2])
+
+# s = "zmrzlina"
+# print(s[4:-1])
+
+# s = "broo"
+
+# print(s[2:])
+# print(s[:2])
+
+
+# s = "butterfly"
+
+# print(s[0:5:2])
+# print(s[5:0:-1])
+
+# s = "slivrussnesharabenchevigem"
+
+# print(s[::-1])
+
+# l = [3, 5, 9, 11, 15]
+# print(l[3])
+# print(l[len(l)-1])
+
+# l = [3, 5, 9, 11, 15]
+# print(l[-1])
+
+# print(77 in [2, 8, 77, 3, 1])
+
+# l = [3, 5, 9, 11, 15]
+# l.append(20)
+# l.append(30)
+# print(l)
+# l += [100, 200, 30]
+# print(l)
+
+# l = [3, 5, 9, 11, 15]
+# l.insert(4, 13)
+# print(l)
+
+# l = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'grape']
+# del l[3]
+# print(l)
+
+# del l[1:3]
+# print
+
+# l = [3, 5, 7, 9]
+# m = l
+# n = l.copy()
+
+
+# print(l == m)
+# print(l == n)
